7edb8e2e50b4e366c8b073528ec2a18b1d0048b7
George Lopez <lopegeor@sheridancollege.ca>
2019-06-04 18:25:03 -0400

    step one

--- prime.py
+++ prime.py
@@ -1,3 +1,20 @@
 """
 prime.py -- Write the application code here
 """
+def generate_prime_factors(number):
+    """
+    Determines all prime factors of a given 'number'.
+    """
+    i = 2
+    factors = []
+    if not isinstance(number, int):
+        raise ValueError
+    while i * i <= number:
+        if number % i:
+            i += 1
+        else:
+            number //= i
+            factors.append(i)
+    if number > 1:
+        factors.append(number)
+    return factors
--- tests/test_generate_prime_factors.py
+++ tests/test_generate_prime_factors.py
@@ -2,3 +2,12 @@
 """
 The test module for Prime Factors
 """
+import pytest
+from prime import generate_prime_factors
+
+def test_invalid_input():
+    """
+    Given non-integer, a ValueError should be raised.
+    """
+    with pytest.raises(ValueError):
+        generate_prime_factors('edward')
