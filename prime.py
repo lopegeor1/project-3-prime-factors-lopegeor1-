65a2dcaea710fc2c65c6552d480d376e22d57129
George Lopez <lopegeor@sheridancollege.ca>
2019-06-04 17:14:48 -0400

    adjustment to prime.py

--- prime.py
+++ prime.py
@@ -1,18 +1,3 @@
 """
 prime.py -- Write the application code here
 """
-def prime_factors(number):
-    """
-    Determines all prime factors of a given 'number'.
-    """
-    i = 2
-    factors = []
-    while i * i <= number:
-        if number % i:
-            i += 1
-        else:
-            number //= i
-            factors.append(i)
-    if number > 1:
-        factors.append(number)
-    return factors
